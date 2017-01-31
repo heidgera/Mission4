dd0b11e4bc2479ecdef5fa3fc0ab8f196d0b9f5c
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2017-01-31 16:52:43 -0600

    configuration setup

--- src/hardware.js
+++ src/hardware.js
@@ -2,16 +2,18 @@ var exec = require('child_process').exec;
 var rpio = require('rpio');
 
 /* Configure P11 as an output pin, setting its initial state to low */
-rpio.open(11, rpio.OUTPUT, rpio.LOW);
+
+//rpio.open(11, rpio.OUTPUT, rpio.LOW);
 rpio.open(13, rpio.OUTPUT, rpio.LOW);
 rpio.open(15, rpio.INPUT, rpio.PULL_DOWN);
 
 function pollcb(pin) {
   if (!rpio.read(pin)) {
     console.log('power removed.');
-    rpio.write(11, rpio.HIGH);
+
+    //rpio.write(11, rpio.HIGH);
     setTimeout(function() {
-      rpio.write(11, rpio.LOW);
+      //rpio.write(11, rpio.LOW);
       exec('sudo shutdown now', function(err, out, sterr) {
         console.log(out);
       });
@@ -22,7 +24,7 @@ function pollcb(pin) {
 rpio.poll(15, pollcb);
 
 process.on('SIGINT', function() {
-  rpio.close(11);
+  //rpio.close(11);
   rpio.close(13);
   rpio.close(15);
   process.exit(0);
@@ -31,3 +33,6 @@ process.on('SIGINT', function() {
 exports.writeOutput = function(state) {
   rpio.write(13, (state) ? rpio.HIGH : rpio.LOW);
 };
+
+// pin@p17 { function = "output"; termination = "pull_up"; polarity = "active_high"; startup_state = "active"; };
+// pin@p26 { function = "output"; termination = "no_pulling"; polarity = "active_low"; startup_state = "inactive"; drive_strength_mA = < 8 >; };
