738ac1e0bd260fda57aafc412e3a1f7461fd0a26
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2017-02-01 13:04:46 -0600

    point to correct questions.json

--- src/questions.js
+++ src/questions.js
@@ -5,12 +5,12 @@ var fs  = require('fs');
 exports.settings = {};
 
 exports.load = () => {
-  let data = fs.readFileSync('questions.json');
+  let data = fs.readFileSync(__dirname + 'questions.json');
   exports.settings = JSON.parse(data);
 };
 
 exports.load();
 
 exports.save = (data) => {
-  fs.writeFileSync('questions.json', JSON.stringify(data));
+  fs.writeFileSync(__dirname + 'questions.json', JSON.stringify(data));
 };
