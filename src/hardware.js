f2c5c901f05d0843e4ef9befc02bd748ffb21325
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2017-02-01 09:08:36 -0600

    probably working

--- src/hardware.js
+++ src/hardware.js
@@ -3,16 +3,16 @@ var rpio = require('rpio');
 
 /* Configure P11 as an output pin, setting its initial state to low */
 
-rpio.init({ mapping: 'gpio' });
+rpio.init({ mapping: 'physical' });
 
-//rpio.open(11, rpio.OUTPUT, rpio.LOW);
-//rpio.open(13, rpio.OUTPUT, rpio.LOW);
-//rpio.open(15, rpio.INPUT, rpio.PULL_DOWN);
+rpio.open(11, rpio.OUTPUT, rpio.LOW);
+rpio.open(13, rpio.OUTPUT, rpio.LOW);
+rpio.open(15, rpio.INPUT, rpio.PULL_DOWN);
 
-rpio.open(24, rpio.OUTPUT, rpio.LOW);
-rpio.open(23, rpio.INPUT, rpio.PULL_OFF);
+rpio.open(18, rpio.OUTPUT, rpio.LOW);
+rpio.open(16, rpio.INPUT, rpio.PULL_OFF);
 
-/*function oldPollCB(pin) {
+function oldPollCB(pin) {
   if (!rpio.read(pin)) {
     console.log('power removed.');
     rpio.write(11, rpio.HIGH);
@@ -23,7 +23,7 @@ rpio.open(23, rpio.INPUT, rpio.PULL_OFF);
       });
     }, 1000);
   }
-}*/
+}
 
 function newPollCB(pin) {
   if (!rpio.read(pin)) {
@@ -36,19 +36,19 @@ function newPollCB(pin) {
   }
 }
 
-//rpio.poll(15, oldPollCB);
-rpio.poll(23, newPollCB);
+rpio.poll(15, oldPollCB);
+rpio.poll(16, newPollCB);
 
 process.on('SIGINT', function() {
-  //rpio.close(11);
-  //rpio.close(13);
-  //rpio.close(15);
-  rpio.close(24);
-  rpio.close(23);
+  rpio.close(11);
+  rpio.close(13);
+  rpio.close(15);
+  rpio.close(18);
+  rpio.close(16);
   process.exit(0);
 });
 
 exports.writeOutput = function(state) {
-  //rpio.write(13, (state) ? rpio.HIGH : rpio.LOW);
-  rpio.write(24, (state) ? rpio.HIGH : rpio.LOW);
+  rpio.write(13, (state) ? rpio.HIGH : rpio.LOW);
+  rpio.write(18, (state) ? rpio.HIGH : rpio.LOW);
 };
