ea8439c78476b1255da8d6ad0a4427b931f10dd9
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2017-05-16 10:13:20 -0500

    changed target

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ sudo dtc -I dts -O dtb -o /boot/dt-blob.bin my-blob.dts
 echo "Installing dependencies for application:"
 
 npm config set disturl https://atom.io/download/atom-shell
-npm config set target 1.3.15
+npm config set target 1.4.15
 npm config set runtime electron
 
 npm i
